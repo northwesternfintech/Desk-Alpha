932d84d0b31a5837e405b3f1c5f17c6737bdbf97
aidanvillasenor <103767625+aidanvillasenor@users.noreply.github.com>
2022-10-29 17:33:31 -0500

    Create testAlgo.py

---
+++ Strats/Template/aidanvillasenor/testAlgo.py
@@ -0,0 +1,48 @@
+import time
+import collections
+import math
+
+#Very simple single-stock template. We will be generating these to get started with a simple strategy, and then generalizing it outward.
+
+class templateStrategy():
+  """
+  A base strategy that is used to explain how to properly develop a strategy.
+  """
+  
+  def __init__(self, metrics, initvalues):
+    """
+    Metrics - the actual information you need to track. If this is a specific algorithm, you can hard-code it and remove that argument.
+    Initvalues - the initial values of your arguments. 
+    """
+    self.data = zip(metrics,initvalues)
+    self.time = time.time()
+    self.orders = []
+    self.ticks = 0
+    self.prev = []
+    self.total_window = 0
+#YOU WILL WANT A FULL SUITE OF SETTER AND GETTER METHODS!
+  
+def clear_orders(self):
+    """
+    Clears all current orders and logs relevant information.
+    """
+    print(f"Trashing %d orders.", len(self.orders))
+    self.orders = []
+    
+def update(self, data):
+    """
+    Will be called on every tick to update the algorithm state and output buys/sells.
+    """
+    self.ticks += 1
+    #Ingest Data
+    updates = zip(data.keys(), data.values())
+    for metric, information in updates:
+      self.data[metric] = information
+      
+    self.clear_orders()
+    #Re-run your logic
+    if len(self.prev) >= 10 and self.data['price'] > self.prev[self.ticks-10]:
+        self.orders.append("BUY")
+    self.prev.append(self.data['price'])
+    #More example logic
+    return self.orders
