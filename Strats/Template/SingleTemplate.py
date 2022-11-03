76085afd573fb26af7e1580bad59793256df9296
Jiarui Cao <jerrycao030111@outlook.com>
2022-11-02 21:25:18 -0500

    hw1 code

--- Strats/Template/SingleTemplate.py
+++ Strats/Template/SingleTemplate.py
@@ -14,12 +14,28 @@ class templateStrategy():
     Metrics - the actual information you need to track. If this is a specific algorithm, you can hard-code it and remove that argument.
     Initvalues - the initial values of your arguments. 
     """
-    self.data = zip(metrics,initvalues)
+    self.data = zip(metrics, initvalues)
     self.time = time.time()
     self.orders = []
     self.ticks = 0
+    self.trend = "upwards"
 #YOU WILL WANT A FULL SUITE OF SETTER AND GETTER METHODS!
   
+def get_data(self):
+  return self.data
+def get_time(self):
+  return self.time
+def get_orders(self):
+  return self.orders
+def get_ticks(self):
+  return self.ticks
+def get_trend(self):
+  return self.trend
+def set_trend(self, newTrend):
+  self.trend = newTrend
+
+
+
 def clear_orders(self):
     """
     Clears all current orders and logs relevant information.
@@ -27,20 +43,33 @@ def clear_orders(self):
     print(f"Trashing %d orders.", len(self.orders))
     self.orders = []
     
+
+
 def update(self, data):
     """
     Will be called on every tick to update the algorithm state and output buys/sells.
     """
     self.ticks += 1
+    prev_price = self.data["price"]
+
     #Ingest Data
     updates = zip(data.keys(), data.values())
+
     for metric, information in updates:
       self.data[metric] = information
-      
     self.clear_orders()
+
     #Re-run your logic
-    if self.data['price'] > 100:
-      self.orders.append('BUY')
+    if self.data['price'] > prev_price:
+      if self.get_trend() == "downwards":
+        self.orders.append('BUY')
+        self.set_trend("upwards")
+
+    elif self.data['price'] < prev_price:
+      if self.get_trend() == "upwards":
+        self.orders.append("SELL")
+        self.set_trend("downwards")
+
     #More example logic
     return self.orders
     
