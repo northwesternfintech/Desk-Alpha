b089331f9850f714837d14762e788798558a6cd7
Jerry Han <109703519+jerry-hannn@users.noreply.github.com>
2022-11-12 20:56:06 -0600

    implement one-liners in header

--- Strats/Template/Jerry-Template/multitemplate.hxx
+++ Strats/Template/Jerry-Template/multitemplate.hxx
@@ -14,22 +14,22 @@ class TemplateStrategy
 
     public:
     //Constructors
-    TemplateStrategy();
+    TemplateStrategy(map<string, map<string, string>> initDict);
 
     //Getters
-    map<string, string> get_orders();
-    map<string, map<string, string> > get_data();
-    size_t get_ticks();
-    time_t get_time();
-    map<string, string> get_data_for_stock(string ticker);
+    map<string, string> get_orders() { return orders; }
+    map<string, map<string, string> > get_data() { return data; }
+    size_t get_ticks() { return ticks; }
+    time_t get_time() { return time; }
+    map<string, string> get_data_for_stock(string ticker) { return data[ticker]; }
 
 
     //Setters
-    void set_data(map<string, map<string, string> >);
-    void set_orders(map<string, string>);
-    void set_ticks(size_t);
-    void set_time(time_t);
-    void set_data_for_stock(string ticker, map<string, string> data);
+    void set_data(map<string, map<string, string> > data) { this->data = data; }
+    void set_orders(map<string, string> orders) { this->orders = orders; }
+    void set_ticks(size_t ticks) { this->ticks = ticks; }
+    void set_time(time_t time) { this->time = time; }
+    void set_data_for_stock(string ticker, map<string, string> data) { this->data[ticker] = data; }
 
     //Methods
     void update_stock(string ticker, map<string, string> data);
