e0dba29e39e92854b0ea4993a3bf6c5000d03b3f
Jerry Han <109703519+jerry-hannn@users.noreply.github.com>
2022-11-18 10:45:12 -0600

    Create bollinger.py

---
+++ Strats/Template/Jerry-Template/bollinger.py
@@ -0,0 +1,12 @@
+import time
+import collections
+import math
+
+#Bollinger Band Template, Single Stock.
+
+
+class bollingerStrategy():
+    """
+    A strategy using a simple moving average, as well as two bands of two standard deviations +- the moving average.
+    """
+    
