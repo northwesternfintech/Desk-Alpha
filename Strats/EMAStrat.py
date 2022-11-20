808123c67984e13c97b9bd0f9d9e51e07b816222
stevenewald <stevenewald6@gmail.com>
2022-11-20 17:47:50 -0600

    Added ema calc

--- Strats/EMAStrat.py
+++ Strats/EMAStrat.py
@@ -45,6 +45,12 @@ def clear_orders(self):
     """
     print(f"Trashing %d orders.", len(self.orders))
     self.orders = []
+
+def calculate_ema(n, p, prev_ema):
+  k = 2/(n+1)
+  new_ema = p*k
+  new_ema += prev_ema*(1-k)
+  return new_ema
     
 def update(self, newPrice):
     """
@@ -56,6 +62,15 @@ def update(self, newPrice):
       
     self.clear_orders()
     #Re-run your logic
+    self.EMA50d = calculate_ema(50, newPrice, self.EMA50d)
+    self.EMA200d = calculate_ema(200, newPrice, self.EMA200d)
+
+    if self.EMA50d<self.EMA200d:
+      self.orders.append("BUY")
+    elif self.EMA50>self.EMA200d:
+      self.orders.append("SELL")
+    else:
+      self.orders.append("HOLD")
 
     
     
