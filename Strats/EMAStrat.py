69fd5f97fb5e69501e0414f6fbaf4338a3d10cbd
Astronicus <astronicusflame@gmail.com>
2022-11-20 18:48:25 -0600

    logic update + initialization with values

--- Strats/EMAStrat.py
+++ Strats/EMAStrat.py
@@ -2,27 +2,41 @@ import time
 import collections
 import math
 
-#Very simple single-stock template. We will be generating these to get started with a simple strategy, and then generalizing it outward.
+#Single-stock template for EMA strategy
 
-class templateStrategy():
+class EMASingleStock():
   """
-  A base strategy that is used to explain how to properly develop a strategy.
+  Single-stock template for overlapping EMA strategy
   """
   
-  def __init__(self, price):
+  def __init__(self):
     """
-    Metrics - the actual information you need to track. If this is a specific algorithm, you can hard-code it and remove that argument.
-    Initvalues - the initial values of your arguments. 
+    Version of init if we have no previous data
     """
     self.price = 0
     self.time = time.time()
-    self.order = []
-    self.ticks = 0
+    self.orders = []
     self.EMA200d = 0
     self.EMA50d = 0
     self.days = 0
+    self.shortOverLong = False
   
-  #YOU WILL WANT A FULL SUITE OF SETTER AND GETTER METHODS!
+  def __init__(self, price, EMA200d, EMA50d):
+    """
+    Version of init if we already have values for EMA200d, EMA50d, and price
+    @type price: float
+    @type EMA200d: float
+    @type EMA50d: float
+    """
+    self.price = price
+    self.time = time.time()
+    self.orders = []
+    self.EMA200d = EMA200d
+    self.EMA50d = EMA50d
+    self.days = 201
+    self.shortOverLong = EMA50d>EMA200d
+  
+  #these are probably just here for template for multi-stock. for one stock can just use self.x
   def get_price(self):
     return self.price
   def get_time(self):
@@ -37,30 +51,37 @@ class templateStrategy():
     return self.EMA200d
   def get_days(self):
     return self.days
-    
-  
-def clear_orders(self):
+
+  def clear_orders(self):
     """
     Clears all current orders and logs relevant information.
     """
     print(f"Trashing %d orders.", len(self.orders))
     self.orders = []
 
-def calculate_ema(n, p, prev_ema):
-  k = 2/(n+1)
-  new_ema = p*k
-  new_ema += prev_ema*(1-k)
-  return new_ema
+  def calculate_ema(days, newPrice, prev_ema):
+    """
+    @type days: int
+    @type newPrice: float
+    @type prev_ema: float
+    @rtype: float
+    """
+    mult = 2/(days+1)
+    new_ema = newPrice*mult
+    new_ema += prev_ema*(1-mult)
+    return new_ema
     
-def update(self, newPrice):
+  def update(self, newPrice):
     """
     Will be called on every tick to update the algorithm state and output buys/sells.
-    @type data: dict
+    @type newPrice: float
     @rtype: list
     """
+    #Increment days and clear orders
     self.days += 1
-      
     self.clear_orders()
+    
+    #Calculate EMA based on how many days are currently updated
     if self.days<50:
       self.EMA50d = calculate_ema(self.days, newPrice, self.EMA50d)
       self.EMA200d = calculate_ema(self.days, newPrice, self.EMA200d)
@@ -68,15 +89,21 @@ def update(self, newPrice):
       self.EMA50d = calculate_ema(50, newPrice, self.EMA50d)
       self.EMA200d = calculate_ema(self.days, newPrice, self.EMA200d)
     else:
-      self.EMA50d = calculate_ema(self.days, newPrice, self.EMA50d)
-      self.EMA200d = calculate_ema(self.days, newPrice, self.EMA200d)
+      self.EMA50d = calculate_ema(50, newPrice, self.EMA50d)
+      self.EMA200d = calculate_ema(200, newPrice, self.EMA200d)
 
-      if self.EMA50d<self.EMA200d:
-        self.orders.append("BUY")
-      elif self.EMA50>self.EMA200d:
-        self.orders.append("SELL")
-      else:
-        self.orders.append("HOLD")
+
+    if self.EMA50d>self.EMA200d: #If our short signal is higher than our long signal, we expect future rises
+      if not(self.shortOverLong) and self.days>250: #When short goes from under long signal to over long signal
+        self.orders.append("BUY")                   #We think prices are low now and will rise in the future, so we buy
+      self.shortOverLong = True
+
+    elif self.EMA50<=self.EMA200d: #If our short signal is lower than our long signal, we expect future dips
+      if self.shortOverLong and self.days>250:       #When short goes from over long signal to under long signal
+        self.orders.append("SELL")                   #We think prices are high now and will lower in the future, so we sell
+      self.shortOverLong = False
+    
+    if len(self.orders) == 0:      #Hold if no order is in orders
+      self.orders.append("HOLD")
 
     return self.orders
-    
