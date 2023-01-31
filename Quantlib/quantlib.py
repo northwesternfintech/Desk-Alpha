5da29ce2f325f28474a26e0c06afed6df009b4e7
Akash Shroff <63399889+akashvshroff@users.noreply.github.com>
2023-01-30 22:55:12 -0600

    math bug fixes

--- Quantlib/quantlib.py
+++ Quantlib/quantlib.py
@@ -2,7 +2,7 @@ class QuantLib:
     def __init__(self):
         pass
 
-    def variance(lst):
+    def variance(self,lst):
         """
         Takes in a list of real numbers and returns their variance or 0 if an empty list is provided. 
         """
@@ -13,6 +13,7 @@ class QuantLib:
             if not isinstance(num, (int, float)):
                 raise "Expected only real numbers in input list"
             mu += num
+        mu = mu/num
         var = 0
         for num in lst:
             var += (num - mu)**2
