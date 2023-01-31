8a565378229535d84191be7be055b9bcb453d745
Akash Shroff <63399889+akashvshroff@users.noreply.github.com>
2023-01-30 22:45:41 -0600

    Added code for variance

--- Quantlib/quantlib.py
+++ Quantlib/quantlib.py
@@ -1,3 +1,20 @@
 class QuantLib:
     def __init__(self):
         pass
+
+    def variance(lst):
+        """
+        Takes in a list of real numbers and returns their variance or 0 if an empty list is provided. 
+        """
+        if lst is None or lst == []:
+            return 0
+        mu = 0
+        for num in lst:
+            if not isinstance(num, (int, float)):
+                raise "Expected only real numbers in input list"
+            mu += num
+        var = 0
+        for num in lst:
+            var += (num - mu)**2
+        var = var/len(lst)
+        return var
