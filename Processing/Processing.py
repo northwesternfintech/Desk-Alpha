9bc0cec64d6b7e19742a3961be4fa2e33b8171b9
Jerry Han <109703519+jerry-hannn@users.noreply.github.com>
2023-05-09 21:21:18 -0500

    Add verbose

--- Processing/Processing.py
+++ Processing/Processing.py
@@ -15,26 +15,34 @@ class Var_sampler:
         self.previous_sample_time = 0
         self.lookback = lookback
         self.sampled = np.array([])
+        self.sampled_times = np.array([])
+        self.variance_arr = np.array([])
 
     def run(self, runtime):
         start_time = time.time()
         while time.time() - start_time < runtime:
             history = self.sampled[-self.lookback:]
+            variance = np.std(history)/np.mean(history)
             if time.time() - self.previous_sample_time >= 1/self.base_rate:
-                self.add_sample()
-            elif (len(self.sampled) > self.lookback) and (np.std(history)/np.mean(history) > self.beta):
-                self.add_sample()
+                self.add_sample(variance)
+            elif (len(self.sampled) > self.lookback) and (variance > self.beta):
+                self.add_sample(variance)
             else:
                 pass
                 
-    def add_sample(self):
+    def add_sample(self, variance):
         self.sampled = np.append(self.sampled, self.sample())
         self.previous_sample_time = time.time()
+        self.sampled_times = np.append(self.sampled_times, self.previous_sample_time)
+        self.variance_arr = np.append(self.variance_arr, variance)
 
 def main():
     vs = Var_sampler(10, 0.5, 0.1, lambda: np.random.normal(100, 10), 10)
-    vs.run(5)
+    vs.run(10)
+    plt.figure(1)
     plt.plot(vs.sampled)
+    plt.figure(2)
+    plt.scatter(vs.variance_arr[:-1], vs.sampled_times[1:] - vs.sampled_times[:-1])
     plt.show()
     print("done")
 
