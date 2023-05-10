822e93b58a432d30486d314d00e509ba04987401
Jerry Han <109703519+jerry-hannn@users.noreply.github.com>
2023-05-09 19:55:36 -0500

    hey it kinda works

--- Processing/Processing.py
+++ Processing/Processing.py
@@ -1,7 +1,7 @@
 import math
-import matplotlib.pyplot as plt
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 
 class Var_sampler:
     # alpha is how much you want variability to matter, measured 0-1.
@@ -16,8 +16,9 @@ class Var_sampler:
         self.lookback = lookback
         self.sampled = np.array([])
 
-    def run(self):
-        while True:
+    def run(self, runtime):
+        start_time = time.time()
+        while time.time() - start_time < runtime:
             history = self.sampled[-self.lookback:]
             if time.time() - self.previous_sample_time >= 1/self.base_rate:
                 self.add_sample()
@@ -30,4 +31,13 @@ class Var_sampler:
         self.sampled = np.append(self.sampled, self.sample())
         self.previous_sample_time = time.time()
 
+def main():
+    vs = Var_sampler(10, 0.5, 0.1, lambda: np.random.normal(100, 10), 10)
+    vs.run(5)
+    plt.plot(vs.sampled)
+    plt.show()
+    print("done")
+
+if __name__ == '__main__':
+    main()
     
